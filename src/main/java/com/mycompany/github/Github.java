76f797b0cf7d53280e7dffd617e537db5b6162fc
Diegolpzn <diego25ln@gmail.com>
2024-03-25 22:56:40 -0500

    Mi modificacion es el soy Diego:)

--- src/main/java/com/mycompany/github/Github.java
+++ src/main/java/com/mycompany/github/Github.java
@@ -13,5 +13,7 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo2");
+        
+        System.out.println("Hola soy Diego");
     }
 }
